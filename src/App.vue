e7934e0d808fd8ee2aefef938233d8d9a2ed31fb
chrikulhan <98656265+chrikulhan@users.noreply.github.com>
2022-11-25 10:24:22 -0600

    try again to push to git with vue components

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <new-student-form></new-student-form>
+    <student-table></student-table>
+    <student-message></student-message>
+
+ </div>
+</template>
+
+<script>
+import NewStudentForm from './components/NewStudentForm.vue'
+import StudentMessage from './components/StudentMessage.vue'
+import StudentTable from './components/StudentTable.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NewStudentForm,
+    StudentMessage,
+    StudentTable
+  },
+//  after the set-up, HERE is where you add data:
+  data() {
+    return {
+    wyrQuestion: 'Would you rather never have to take a bath/shower but still always smell nice or never have to get ' +
+        'another shot but still be healthy?',
+      wyrAnswer1: 'No bathing, but always smell nice.',
+      wyrAnswer2: 'No shots, but still be healthy.',
+      userSelectionMessage: ''
+   //
+    }
+  },
+  methods: {
+    answerChanged(choice) {
+      this.userSelectionMessage = `Thanks! You chose ${choice}`
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/StudentMessage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <!--    template here-->
+  </div>
+</template>
+
+<script>
+export default {
+  //create the component here:
+  //whatever is created here "export" will be available to another
+  //  javascript will read this file and "export default" means whatever is created
+  //  here will be available to that other file.
+  name: "StudentMessage.vue"
+}
+</script>
+
+<style scoped>
+/*scoped means these component styles will only apply here, not on other components. */
+
+</style>
